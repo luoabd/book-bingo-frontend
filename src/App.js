8a6b98cff6b0289b71a2d6782bfaa4d22abaf0c2
luoabd <abdellah.lahnaoui@gmail.com>
2023-02-08 02:18:06 +0100

    Add simple usage instructions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import Export from "./Export";
 import Clear from "./Clear";
+import Info from "./Info";
 import "./App.css";
 
 function App() {
@@ -37,11 +38,35 @@ function App() {
     localStorage.setItem("data", JSON.stringify(metaData));
   }, [metaData]);
 
+  const [showInfo, setShowInfo] = useState(() => {
+    const saved = localStorage.getItem("info");
+    const initialValue = JSON.parse(saved);
+    return initialValue || false;
+  });
+  useEffect(() => {
+    localStorage.setItem("info", JSON.stringify(showInfo));
+  }, [showInfo]);
+
   return (
     <div className="container my-12 mx-auto px-4 md:px-12">
       <header className="flex justify-center pb-6">
         <h1 className="text-3xl underline font-bold"> Book Bingo 2023 </h1>
+        <button
+          className="ml-4 pl-2 bg-coolor-2 rounded-2xl"
+          onClick={() => setShowInfo(!showInfo)}
+        >
+          <svg
+            className="fill-current w-4 h-4 mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
+          </svg>
+        </button>
       </header>
+      <div className={showInfo ? "hidden" : "block"}>
+        <Info />
+      </div>
       <div class="flex flex-wrap -mx-1 lg:-mx-4">
         <Card
           stateChanger={setMetaData}
