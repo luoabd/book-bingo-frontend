3363771cafddada346f8d9604d4803eb40a58532
luoabd <abdellah.lahnaoui@gmail.com>
2023-03-02 19:12:15 +0100

    Convert to Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import FullyBooked from "./pages/FullyBooked";
 import RFantasy from "./pages/RFantasy";
 import Home from "./pages/Home";
@@ -8,14 +8,14 @@ import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
         <Route index element={<Home />} />
         <Route path="fullybooked" element={<FullyBooked />} />
         <Route path="rfantasy" element={<RFantasy />} />
         <Route path="*" element={<ErrorPage />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
   )
 }
 
