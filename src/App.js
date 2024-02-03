391be5495bd1ccf94d687226cd967481a0865c80
Abdellah L <18109349+luoabd@users.noreply.github.com>
2024-02-03 22:08:59 +0100

    feat: Add support for chaos bongo (and remove FB23) (#1)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import FullyBooked from "./pages/FullyBooked";
+import Bongo24 from "./pages/Bongo24";
 import FullyBooked24 from "./pages/FullyBooked24";
 import RFantasy from "./pages/RFantasy";
 import Home from "./pages/Home";
@@ -12,8 +12,8 @@ function App() {
     <HashRouter>
     <Routes>
         <Route index element={<Home />} />
-        <Route path="fullybooked" element={<FullyBooked />} />
         <Route path="fullybooked24" element={<FullyBooked24 />} />
+        <Route path="bongo24" element={<Bongo24 />} />
         <Route path="rfantasy" element={<RFantasy />} />
         <Route path="*" element={<ErrorPage />} />
     </Routes>
